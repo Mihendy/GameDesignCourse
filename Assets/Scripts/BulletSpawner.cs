bff9314fd99376b3723d602e131a5b76c9f58aa9
mihendy <kryukovm1khael@yandex.ru>
2025-06-26 23:19:04 +0500

    WorkShop7

--- Assets/Scripts/BulletSpawner.cs
+++ Assets/Scripts/BulletSpawner.cs
@@ -1,9 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletSpawner : MonoBehaviour
 {
     public GameObject BulletPrefab;
+    AudioSource bulletSound;
     public float BulletVelocity = 20f;
+
+    void Start()
+    {
+        bulletSound = GetComponent<AudioSource>();
+    }
+    void StopBulletSound() => bulletSound.Stop();
 
     void Update()
     {
@@ -13,6 +22,11 @@ public class BulletSpawner : MonoBehaviour
                 BulletPrefab, transform.position, transform.rotation);
             newBullet.GetComponent<Rigidbody>().linearVelocity =
                 transform.forward * BulletVelocity;
+
+            bulletSound.time = 0.5f;
+            bulletSound.Play();
+            Invoke(nameof(StopBulletSound), 0.5f);
         }
     }
+
 }
